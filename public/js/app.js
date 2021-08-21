484ba2640d0dfc5bc2954989673cc75d5be9ffb0
dingsheng96 <dingshengho@cspeed.com.my>
2021-08-22 01:41:09 +0800

    recompile app

--- public/js/app.js
+++ public/js/app.js
@@ -93192,8 +93192,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel\rhinosite\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel\rhinosite\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\laravel\bizboo-web\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel\bizboo-web\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
