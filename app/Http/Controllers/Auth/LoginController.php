be9d910016a0c28eb0d2a3d8a73d9b0f7712e59d
dingsheng96 <dingshengho@cspeed.com.my>
2021-09-03 11:17:55 +0800

    reset default auth file

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,13 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -26,7 +21,12 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    public $redirectTo = RouteServiceProvider::HOME;
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -36,87 +36,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    /**
-     * Get the needed authorization credentials from the request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    protected function credentials(Request $request)
-    {
-        $credentials = array_merge(
-            $request->only($this->username(), 'password'),
-            ['status' => User::STATUS_ACTIVE, 'application_status' => User::APPLICATION_STATUS_APPROVED]
-        );
-
-        return $credentials;
-    }
-
-    /**
-     * The user has been authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  mixed  $user
-     * @return mixed
-     */
-    protected function authenticated(Request $request, $user)
-    {
-        if ($user->is_member || $user->is_guest) {
-
-            $this->guard()->logout();
-
-            $request->session()->invalidate();
-            $request->session()->regenerateToken();
-
-            throw ValidationException::withMessages([
-                $this->username() => [trans('auth.failed')],
-            ]);
-
-            return redirect()->route('login');
-        }
-
-        $message =  $user->name . ' ' . strtolower(__('messages.login_success'));
-
-        activity()->useLog('web:auth')
-            ->causedByAnonymous()
-            ->performedOn($user)
-            ->withProperties($request->all())
-            ->log($message);
-
-        return redirect()->route('dashboard');
-    }
-
-    /**
-     * Log the user out of the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
-    public function logout(Request $request)
-    {
-        $user = Auth::user();
-
-        $message = $user->name . ' ' . strtolower(__('messages.logout_success'));
-
-        activity()->useLog('web:auth')
-            ->causedBy($user)
-            ->withProperties($request->all())
-            ->log($message);
-
-        $this->guard()->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        if ($response = $this->loggedOut($request)) {
-            return $response;
-        }
-
-        return $request->wantsJson()
-            ? new JsonResponse([], 204)
-            : redirect()->route('login')->withSuccess($message);
     }
 }
